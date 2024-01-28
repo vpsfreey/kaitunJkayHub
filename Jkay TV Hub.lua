84916e962e23855025a824ca175c118c326e6e32
vpsfreey <141880862+vpsfreey@users.noreply.github.com>
2024-01-29 02:01:14 +0700

    Jkay TV Hub.lua

--- Jkay TV Hub.lua
+++ Jkay TV Hub.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 --------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
     Title = "Jkay Hub",
-    SubTitle = "by Tên Thằng Skid",
+    SubTitle = "by Jkay Hub",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
